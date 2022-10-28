b4ad6fd2c5f57d2a1e53625dd30474051822a247
6 6 6 <chomiczjakub80@gmail.com>
2022-10-28 20:06:54 +0200

    Revert "Init"
    This reverts commit eb7d7840b5221c2a227d39e789bf32c144c0e1da.
    
    # Conflicts:
    #	.gitmodules

--- PlayerController/shaders/compile-spirv.bat
+++
@@ -1,2 +0,0 @@
-D:\VulkanSDK\1.3.216.0\Bin\glslc.exe shader.vert -o vertex.spv
-D:\VulkanSDK\1.3.216.0\Bin\glslc.exe shader.frag -o fragment.spv
--- build/Win-GenProjects.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-pushd ..\
-call premake5.exe vs2022
-popd
-PAUSE
